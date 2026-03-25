4506e812c67de951e2f10d721a6efcb867a80ed5
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-03-25 22:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if uploaded_file is not None:
     img_bgr = cv2.imdecode(file_bytes, 1)
     img_rgb_raw = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
     # STEP 1: Resize to 1200px
-    img_input = pre_process_resize(img_rgb, target_width=1200)
+    img_input = pre_process_resize(img_rgb_raw, target_width=1200)
 
     # STEP 2: AI Enhancement
     with st.spinner("AI is working..."):
