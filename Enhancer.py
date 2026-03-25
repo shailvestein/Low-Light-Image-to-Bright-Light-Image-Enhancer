0e99b14279078275d1cc6ba7e19c5fea18901194
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-03-25 20:31:56 +0530

    Update Enhancer.py

--- Enhancer.py
+++ Enhancer.py
@@ -1,4 +1,3 @@
-    
 import torch
 import numpy as np
 import cv2
@@ -63,7 +62,7 @@ class Enhancer:
     
     def enhance_image(self, img):
 		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        patch_size = 512
+        patch_size = 512 # Run inference over this patch size
         stride = 256  # Essential 50% overlap for spline blending
         h, w, _ = img.shape
     
