ed1280019a9c0e970e2d5e0d6b02dc39776693df
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-03-27 23:10:36 +0530

    Update Enhancer.py

--- Enhancer.py
+++ Enhancer.py
@@ -8,7 +8,9 @@ class Enhancer:
     def __init__(self, model1, model2, batch_size):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model1 = model1.to(self.device)
+        self.model1.eval()
         self.model2 = model2.to(self.device)
+        self.model2.eval()
         self.batch_size = batch_size
     
     def get_ultra_sharp_mask(self, patch_size, fade_width=64):
@@ -78,7 +80,7 @@ class Enhancer:
         with torch.no_grad():
             for batch in loader:
                 out = self.model1(batch.to(self.device))
-                out = self.model2(out)
+                R, L, out = self.model2(out)
                 enhanced_list.extend([p.cpu() for p in out])
         output = self.combine_tensor_patches(enhanced_list, coords, (h, w), (nh, nw), patch_size)
         return output, time.time()-start_time
