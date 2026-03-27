a12c4b2609d54eacff7ef2fa302293a50b03e264
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-03-28 00:08:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ if uploaded_file is not None:
         st.write("🧪 Analyzing scene lighting...")
         enhc_img, pt1 = enhancer_1.enhance_image(img_input)
         enhc_img = torch.clamp(enhc_img, 0, 1)
-        # enhc_img, pt2 = enhancer_1.enhance_image(enhc_img)
-        enhc_img = cv2.cvtColor(enhc_img, cv2.COLOR_BGR2RGB)
+        # enhc_img, pt2 = enhancer_2.enhance_image(enhc_img)
+        # enhc_img = cv2.cvtColor(enhc_img, cv2.COLOR_BGR2RGB)
         enhc_img = torch.clamp(torch.from_numpy(enhc_img).float(), 0,1)
         enhc_img = enhc_img.numpy()  
         p_time = pt1 + 0
