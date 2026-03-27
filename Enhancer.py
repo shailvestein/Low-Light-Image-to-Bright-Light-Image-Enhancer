0a22489b9435b1f19c848e6281c72aaa8ad41bb1
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-03-28 01:52:42 +0530

    Update Enhancer.py

--- Enhancer.py
+++ Enhancer.py
@@ -34,7 +34,7 @@ class Enhancer:
         canvas = torch.zeros((3, nh, nw), dtype=torch.float32)
         weight_sum = torch.zeros((1, nh, nw), dtype=torch.float32)
         
-        mask = self.get_ultra_sharp_mask(patch_size, fade_width=8)
+        mask = self.get_ultra_sharp_mask(patch_size, fade_width=32)
         
         for idx, (i, j) in enumerate(coords):
             # Patch format: (C, H, W)
@@ -73,8 +73,8 @@ class Enhancer:
     def enhance_image(self, img):
         start_time = time.time()
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        patch_size = 512 # Run inference over this patch size
-        stride = 256  # Essential 50% overlap for spline blending
+        patch_size = 1024 # Run inference over this patch size
+        stride = 512  # Essential 50% overlap for spline blending
         h, w, _ = img.shape
         # Padding to match stride logic
         pad_h = (patch_size - h % stride) % stride + (patch_size - stride)
