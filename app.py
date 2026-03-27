c3bf10ddfa1c31feff6bb6fd7cf14e0ee2c29a44
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-03-28 00:05:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ if uploaded_file is not None:
     with st.status("🚀 AI Engine is working...", expanded=True) as status:
         st.write("🧪 Analyzing scene lighting...")
         enhc_img, pt1 = enhancer_1.enhance_image(img_input)
-        # enhc_img = torch.clamp(enhc_img, 0, 1)
+        enhc_img = torch.clamp(enhc_img, 0, 1)
         # enhc_img, pt2 = enhancer_1.enhance_image(enhc_img)
         enhc_img = cv2.cvtColor(enhc_img, cv2.COLOR_BGR2RGB)
         enhc_img = torch.clamp(torch.from_numpy(enhc_img).float(), 0,1)
