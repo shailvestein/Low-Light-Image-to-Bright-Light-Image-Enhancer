2ca7704753240dd2986b1cf83e4168adb8f918bc
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-03-29 02:07:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ if uploaded_file is not None:
         with st.status("🚀 AI Engine is working...", expanded=True) as status:
             # enhc_img, p1 = enhancer_1.enhance_image(img_input)
             # emhc_img = enhc_img * 255
-            enhc_img, p2 = enhancer_2.enhance_image(enhc_img)
+            enhc_img, p2 = enhancer_2.enhance_image(img_input)
             emhc_img = enhc_img * 255
             enhc_img = cv2.cvtColor(enhc_img, cv2.COLOR_BGR2RGB)
             p_time = p2
